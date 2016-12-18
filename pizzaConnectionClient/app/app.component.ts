f682c485f6cd4974952cf4e9fea65b89e3e67d0d
RichiKo <richi-86@gmx.de>
2016-12-18 20:24:48 +0100

    add config files to start app

---
+++ pizzaConnectionClient/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'pizzaconnection',
+    templateUrl: 'app/app.html',
+    styleUrls: []
+})
+
+export class AppComponent {
+
+}
+
