1f0163742b8b2bc2fc99f4c0632c2e9437b80584
Richard Kolyadko <rkolyadko@vwd.com>
2016-12-21 09:22:59 +0100

    add new pages

---
+++ pizzaConnectionClient/app/pizza/ts/components/pizzalist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pizzalist',
+  templateUrl: 'app/pizza/templates/pizzalist.html',
+  styleUrls: []
+})
+
+export class PizzaListComponent { }
