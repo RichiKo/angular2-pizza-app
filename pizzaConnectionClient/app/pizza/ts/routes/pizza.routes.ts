0945ca84d29070c47900d324da7d76895fad9b65
Richard Kolyadko <rkolyadko@vwd.com>
2016-12-21 09:29:59 +0100

    add routings to pages

---
+++ pizzaConnectionClient/app/pizza/ts/routes/pizza.routes.ts
@@ -0,0 +1,6 @@
+import { PizzaListComponent } from '../components/pizzalist.component';
+import { Routes } from '@angular/router';
+
+export const HomeRoutes: Routes = [
+  { path: 'pizzalist', component: PizzaListComponent }
+];
